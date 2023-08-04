a4e9b06a14ce6b4a078c8bcda99d6ae1af56b93a
mich-wierzch <theordinarymaiky@gmail.com>
2023-08-04 16:23:27 +0200

    fixed image uploading

--- src/main/java/com/CatShelter/CatShelter/controller/PostController.java
+++ src/main/java/com/CatShelter/CatShelter/controller/PostController.java
@@ -4,9 +4,7 @@ import com.CatShelter.CatShelter.dto.PostDto;
 import com.CatShelter.CatShelter.service.PostService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.List;
 @RequestMapping(path="/api/posts")
 @AllArgsConstructor
--- src/main/java/com/CatShelter/CatShelter/service/PostService.java
+++ src/main/java/com/CatShelter/CatShelter/service/PostService.java
@@ -11,11 +11,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.time.LocalDate;
-import java.util.Base64;
 import java.util.List;
 import java.util.stream.Collectors;
 
