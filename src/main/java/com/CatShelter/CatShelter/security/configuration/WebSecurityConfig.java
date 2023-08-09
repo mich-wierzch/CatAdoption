a868923c2f271d46213ad8fe02de3168312a9a56
mich-wierzch <theordinarymaiky@gmail.com>
2023-08-10 00:00:45 +0200

    updated session timeout

--- src/main/java/com/CatShelter/CatShelter/security/configuration/WebSecurityConfig.java
+++ src/main/java/com/CatShelter/CatShelter/security/configuration/WebSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.session.SessionInformationExpiredStrategy;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -96,4 +97,5 @@ public class WebSecurityConfig {
         return source;
 
     }
+
 }
