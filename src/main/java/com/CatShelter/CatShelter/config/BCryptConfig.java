d8109e7d57a56dd6f194dcee8d1bdd89976a6c28
mich-wierzch <theordinarymaiky@gmail.com>
2023-08-12 17:22:19 +0200

    moved bcrypt config

--- src/main/java/com/CatShelter/CatShelter/config/BCryptConfig.java
+++ src/main/java/com/CatShelter/CatShelter/config/BCryptConfig.java
@@ -1,11 +1,11 @@
-package com.CatShelter.CatShelter.security;
+package com.CatShelter.CatShelter.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-public class PasswordEncoder {
+public class BCryptConfig {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
         return new BCryptPasswordEncoder();
